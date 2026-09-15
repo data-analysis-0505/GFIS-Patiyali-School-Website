799f48975ca9030ff4c6c1d7c6e0a55c8e4f1d9b
data-analysis-0505 <abhineetyadav0505@gmail.com>
2026-09-16 01:13:03 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
+```
